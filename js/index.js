8146a770746820b6fdb065296b52f3f7771c7f50
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2019-11-10 20:01:52 -0300

    algumas coisinhas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 
+let API = "http://localhost:8080/v1/api"
+
+function carregando(){
+    let $body = document.querySelector(".centro");
+    let $div = document.createElement("div")
+    $body.appendChild($div);
+    $div.innerHTML = "Adicionando usuario..."
+    setTimeout(()=> {$body.removeChild($div)},3000);
+}
+
 function save(){
     let nome = document.querySelector(".Nome input").value;
     let ultimoNome = document.querySelector(".UltimoNome input").value
@@ -7,7 +17,16 @@ function save(){
     let senha = document.querySelector(".Senha input").value
     console.log(nome)
     let json = `{"nome": "${nome}","ultimoNome": "${ultimoNome}","email":"${email}","cartaoDeCredito":"${cartao}","senha":"${senha}"}`;
-    console.log(json)
+    
+    fetch(API + "/usuarios" , {
+        'method':'POST',
+        'body': json,
+        'headers' : {'Content-Type':'application/json'}
+    }).then(r => r.json())
+    .then(j => {
+        console.log(j)
+        carregando()
+    })
 }
 
 
